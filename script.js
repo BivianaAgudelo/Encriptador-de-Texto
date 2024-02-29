616f5d1e22a6ba197239026ef85eb133624fcfb2
Bima <biviana_agudelo@hotmail.com>
2024-02-29 13:38:45 -0500

    creación de funciones para encriptar y desencriptar texto

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-const campoIngresarTexto = document.querySelector("#ingresar-texto");
-const campoEncriptarTexto = document.querySelector("#campo-encriptado");
+const campoIngresarTexto = document.querySelector(".text-area");
+const campoEncriptarTexto = document.querySelector(".mensaje");
 
+/** Ocultar imagen textarea robot */
+var contenedorEncriptador = document.querySelector(".img-robot-encriptador");
+var imagenEncriptador = contenedorEncriptador.querySelector("img");
 
+/** Ocultar mensaje "Ningún mensaje fue
+    encontrado
+    Ingresa el texto que desees encriptar o desencriptar" 
+*/
+
+var mensajeNoEncontrado = document.querySelector(".mensaje-no-encontrado");
 
 const matrizCode = [
-    ["a", "ai"],
-    ["e", "enter"],
-    ["i", "imes"],
-    ["o", "ober"],
-    ["u", "ufat"]
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"],
 ];
 
 function btnEncriptar() {
-    const texto = encriptar(campoIngresarTexto.value);
-    campoEncriptarTexto.value = texto;
+  const texto = encriptar(campoIngresarTexto.value);
+  campoEncriptarTexto.value = texto;
+  campoIngresarTexto.value = "";
+  imagenEncriptador.style.display = "none";
+  mensajeNoEncontrado.style.display = "none";
 }
 
 function encriptar(fraseEncriptada) {
-    for(let i = 0; i < matrizCode.length; i++) {
-        if(fraseEncriptada.includes(matrizCode[i][0])) {
-            fraseEncriptada = fraseEncriptada.replaceAll (
-                matrizCode [i][0],
-                matrizCode [i][1]
-            )
-        }
+  fraseEncriptada = fraseEncriptada.toLowerCase();
+  for (let i = 0; i < matrizCode.length; i++) {
+    if (fraseEncriptada.includes(matrizCode[i][0])) {
+      fraseEncriptada = fraseEncriptada.replaceAll(
+        matrizCode[i][0],
+        matrizCode[i][1]
+      );
     }
-    return fraseEncriptada;
+  }
+  return fraseEncriptada;
 }
 
+function btnDesencriptar() {
+  const texto = desencriptar(campoIngresarTexto.value);
+  campoEncriptarTexto.value = texto;
+  campoIngresarTexto.value = "";
+}
+
+function desencriptar(fraseDesencriptada) {
+  fraseDesencriptada = fraseDesencriptada.toLowerCase();
+  for (let i = 0; i < matrizCode.length; i++) {
+    if (fraseDesencriptada.includes(matrizCode[i][1])) {
+      fraseDesencriptada = fraseDesencriptada.replaceAll(
+        matrizCode[i][1],
+        matrizCode[i][0]
+      );
+    }
+  }
+  return fraseDesencriptada;
+}
